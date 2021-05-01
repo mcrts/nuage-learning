094585bf0a7e9da51ebe9c305e60478790792ebb
Paul Andrey <paul.andrey@chru-lille.fr>
2021-05-01 18:43:19 +0200

    Small steps towards Server implementation.

--- example.py
+++ example.py
@@ -5,23 +5,33 @@ import time
 
 from src.client import Client
 from src.server import Server
+from src.model import FederatedSGDClassifier
+
+from sklearn.datasets import load_iris
+
 
 SERVER = '192.168.1.17:9092'
 
 if __name__ =='__main__':
+    data = load_iris(return_X_y=True)
+    model = FederatedSGDClassifier(n_classes=3, n_features=4)
+
     client = Client(
-        dataset='mydata',
-        groupid='groupid',
-        model='model',
+        dataset=data,
+        groupid='client.001',
+        model=model,
         server=SERVER
     )
+    
     server = Server(
         n_clients=2,
         groupid='server',
         model='model',
         server=SERVER
     )
-    server.initialize()
+    #server.initialize()
+
+    client.run_once()
     #th1 = threading.Thread(target=client.run, daemon=True)
     #th1.start()
     #time.sleep(10)
