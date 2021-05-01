178a68670e7bf36b2d1787a85c248c8747db469d
Paul Andrey <paul.andrey@chru-lille.fr>
2021-05-01 18:01:36 +0200

    Fixed SGDClassifier and added a method to produce 'fake' weights.

--- src/model/_sgd_classif.py
+++ src/model/_sgd_classif.py
@@ -3,18 +3,20 @@
 """Simple Gradient Descent (SGD) classifier model implementation."""
 
 import inspect
-from abc import ABCMeta, abstractmethod
 
+import numpy as np
 import sklearn
 from sklearn.linear_model import SGDClassifier
 
+from src.model import FederatedModel
 
-class FederatedSGDClassifier(metaclass=ABCMeta):
+class FederatedSGDClassifier(FederatedModel):
     """Simple Gradient Descent (SGD) classifier model."""
 
     def __init__(
             self,
             n_classes,
+            n_features,
             loss='log',
             penalty='l2',
             alpha=1e-4,
@@ -29,6 +31,7 @@ class FederatedSGDClassifier(metaclass=ABCMeta):
         ):
         """Instantiate the SGD-based classifier model."""
         self.n_classes = n_classes
+        self.n_features = n_features
         self.model = SGDClassifier(
             loss=loss, penalty=penalty, alpha=alpha, l1_ratio=l1_ratio,
             fit_intercept=fit_intercept, epsilon=epsilon, n_jobs=n_jobs,
@@ -36,17 +39,15 @@ class FederatedSGDClassifier(metaclass=ABCMeta):
             class_weight=class_weight
         )
 
-    @abstractmethod
     def get_config(self):
         """Return a JSON-serializable dict enabling re-instantiation."""
         keys = inspect.signature(type(self)).parameters.keys()
-        config = {'n_classes': self.n_classes}
+        config = {'n_classes': self.n_classes, 'n_features': self.n_features}
         config.update({
             key: val for key, val in self.model.get_params() if key in keys
         })
         return config
 
-    @abstractmethod
     def get_weights(self):
         """Return the model's trainable parameters."""
         weights = {'coef_': getattr(self.model, 'coef_', None)}
@@ -54,7 +55,6 @@ class FederatedSGDClassifier(metaclass=ABCMeta):
             weights['intercept_'] = getattr(self.model, 'intercept_', None)
         return weights
 
-    @abstractmethod
     def set_weights(self, weights):
         """Update the model's trainable parameters to input values."""
         if 'coef_' not in weights:
@@ -65,13 +65,22 @@ class FederatedSGDClassifier(metaclass=ABCMeta):
                 raise KeyError("Missing 'weights' key: 'intercept_'")
             self.model.intercept_ = weights['intercept_']
 
-    @abstractmethod
+    def generate_weights(self, random=True, seed=None):
+        if random:
+            rng = np.random.default_rng(seed=seed)
+            get_weights = lambda size: rng.normal(size=size).astype(np.float32)
+        else:
+            get_weights = lambda size: np.zeros(shape=size, dtype=np.float32)
+        weights = {'coef_': get_weights((self.n_features,))}
+        if self.model.fit_intercept:
+            weights['intercept_'] = get_weights((self.n_classes,))
+        return weights
+
     def run_training_epoch(self, X, y, sample_weight=None):
         """Train the model on a given dataset for one epoch."""
         classes = list(range(self.n_classes))
         self.model.partial_fit(X, y, classes, sample_weight)
 
-    @abstractmethod
     def evaluate(self, X, y, sample_weight=None):
         """Evaluate the model on a given dataset."""
         accuracy = self.model.score(X, y, sample_weight)
